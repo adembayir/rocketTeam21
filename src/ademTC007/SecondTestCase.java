2ac67ba6ff22b2cfd51b6782960db7036fc40845
Adem BAYIR <adembyr@hotmail.com>
2022-04-27 22:58:46 +0300

    New test case completed.

--- src/ademTC007/SecondTestCase.java
+++ src/ademTC007/SecondTestCase.java
@@ -5,5 +5,7 @@ public class SecondTestCase {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+
+        System.out.println("Test case completed"); // amend example
     }
 }
