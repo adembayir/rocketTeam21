7700a0081e0fe9a6e51ca22ff76e1c3899b3a847
savci35 <ebrusavas2525@gmail.com>
2022-04-27 22:58:36 +0300

    3.commit

--- src/savcı/Savcı2.java
+++ src/savcı/Savcı2.java
@@ -4,5 +4,6 @@ public class Savcı2 {
     public static void main(String[] args) {
         System.out.println("Savcı");
         System.out.println("Rocket");
+        System.out.println("Rocket1");
     }
 }
