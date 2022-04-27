e7fbc182596e0ca339186fad4b3507365a1406e9
Yasin <yasin.deger48@gmail.com>
2022-04-27 22:06:26 +0300

    First commit HelloWorld

--- src/yasin/Yasin.java
+++ src/yasin/Yasin.java
@@ -5,5 +5,7 @@ public class Yasin {
     public static void main(String[] args) {
 
         System.out.println("Cool cool cool");
+
+        System.out.println("Hello world hello world hello world");
     }
 }
