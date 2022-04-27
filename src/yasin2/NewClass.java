fb7db8947feefd97b8a7fb692f30b7d6948cba10
Yasin <yasin.deger48@gmail.com>
2022-04-27 22:58:18 +0300

    Second commit newClass

--- src/yasin2/NewClass.java
+++ src/yasin2/NewClass.java
@@ -5,6 +5,8 @@ public class NewClass {
     public static void main(String[] args) {
         System.out.println("Cool cool cool");
 
+        System.out.println("Hello world hello world hello world");
+
     }
 
 }
