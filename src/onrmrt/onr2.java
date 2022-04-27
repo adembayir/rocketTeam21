f6228e145447d1f13d49e9b315fd2310d416d9e4
Onurmk99 <onurmkoprulu@gmail.com>
2022-04-27 23:43:20 +0300

    Sixth  commit completed

--- src/onrmrt/onr2.java
+++ src/onrmrt/onr2.java
@@ -4,5 +4,8 @@ public class onr2 {
     public static void main(String[] args) {
         System.out.println("I came back");
         System.out.println("It is all done");
+        System.out.println("son son son");
+
+
     }
 }
